a63c7f700601e8cc2c77129ba161eda4161a885c
Brandon Duncan <bduncan@altriva.com>
2022-09-12 12:14:07 -0700

    New azure function which hosts endpoints for the survey data integration
    These endpoints are called from synapse data pipelines and are used to parse through JSON and convert the data to SQL-friendly JSON records which the pipelines insert into the SQL DB.

---
+++ TrailBlazers.Qualtrics.SurveyResponseDataIntegration/TrailBlazers.Qualtrics.SurveyResponseExportToSql/Models/SurveyQuestionModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrailBlazers.Qualtrics.SurveyResponseExportToSql.Models
+{
+    public class SurveyQuestionModel
+    {
+        public string Id { get; set; }
+        public string QuestionId { get; set; }
+        public string SurveyId { get; set; }
+        public string QuestionDescription { get; set; }
+        public string QuestionText { get; set; }
+        public string QuestionType { get; set; }
+        public string Selector { get; set; }
+        public string SubSelector { get; set; }
+        public string MatrixOption { get; set; }
+    }
+}
